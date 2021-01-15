74ff0ddabf15972a8a30afecba3ea4f3c59549fc
VEGALUISJ <luisvega.jlvv@gmail.com>
2021-01-15 02:25:23 -0500

    new php postmethhod

--- configcontact.php
+++ configcontact.php
@@ -1,23 +1,15 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+    
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $subject = $_POST['subject'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
 
-    $email_from = 'luisvega.jlvv@gmail.com';
+        $mailTo = "jose.vega@codeninjas.com";
+        $headers = "From :".$mailFrom;
+        $txt = "You have received an email from ".$name."\n\n".$message;
 
-    $email_subject = "New Form Submission";
-
-    $email_body = "User Name: $name.\n".
-                    "User Email $visitor_email.\n".
-                        "User_Message: $message.\n";
-
-    $to = "luisvega.jlvv@gmail.com"
-
-    $headers = "From : $email_from \r\n";
-
-    $headers = "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    headers("Location: index.html");
-?>
+        mail($mailTo, $subject, $txt, $headers);
+        header("Location: index.php?mailsend")
+    }
