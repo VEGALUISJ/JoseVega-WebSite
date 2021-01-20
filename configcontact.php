5d0238a67b5cea4c290ebd53b13a4ad0d3e43e4c
VEGALUISJ <luisvega.jlvv@gmail.com>
2021-01-20 12:36:57 -0500

    fix on border and Href links color

--- configcontact.php
+++ configcontact.php
@@ -11,5 +11,5 @@
         $txt = "You have received an email from ".$name."\n\n".$message;
 
         mail($mailTo, $subject, $txt, $headers);
-        header("Location: index.php?mailsend")
+        header("Location: index.php?mailsend");
     }
